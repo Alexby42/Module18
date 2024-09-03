f45ed1cf8a90d07b66188b07d69058971e102eda
Alexby42 <163275700+Alexby42@users.noreply.github.com>
2024-09-03 21:57:41 +0700

    Urls_Views

--- UrbanDjango/urls.py
+++ UrbanDjango/urls.py
@@ -16,7 +16,11 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
-
+from django.views.generic import TemplateView
+from UrbanDjango.task2.views import index
+from UrbanDjango.task2.views import index2
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', index),
+    path('index/', index2.as_view())
 ]
